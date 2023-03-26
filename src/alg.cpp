6da8beb646a9e136ed11afb56605652916ec19fc
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-03-26 14:18:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,60 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int scum = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                scum = scum + 1;
+            }
+
+        }
+    }
+    return scum;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int scum = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = len - 1; i < j; --j) {
+            if (arr[i] + arr[j] == value) {
+                scum = scum + 1;
+            }
+        }
+    }
+    return scum;
+}
+int Poisc(int* arr, int l, int r, int value) {
+    int scum = 0;
+    int mid = 0;
+    int temp = 0;
+    while (l < r) {
+        mid = (l + r) / 2;
+        if (arr[mid] == value) {
+            scum = scum + 1;
+            temp = mid + 1;
+            while (arr[temp] == value && temp < r) {
+                scum = scum + 1;
+                temp = temp + 1;
+            }
+            temp = mid - 1;
+            while (arr[temp] == value && temp > l) {
+                scum = scum + 1;
+                temp = temp - 1;
+            }
+            break;
+        }
+        if (arr[mid] < value) {
+            l = mid;
+        }
+        if (arr[mid] > value) {
+            r = mid;
+        }
+    }
+    return scum;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int scum = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        scum = scum + Poisc(arr, i, len, value - arr[i]);
+    }
+    return scum;
 }
