5b21644bc4e7eeea6b119006a5987e708d92deab
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-03-26 14:35:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,11 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int scum = 0;
-    for (int i = 0; i < len; ++i){
+    for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
             if (arr[i] + arr[j] == value) {
                 scum = scum + 1;
             }
-
         }
     }
     return scum;
